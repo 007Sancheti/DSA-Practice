e91bb38dce1f4c724661e5a92e4ce6aa9c3831b2
Abhinav Sancheti <001sancheti@gmail.com>
2021-06-06 13:45:06 +0530

    max-min completed

---
+++ Array/maximum-minimum.js
@@ -0,0 +1,16 @@
+var maxMin = (arr) => {
+    let max = arr[0];
+    let min = arr[0];
+    for(let i=0; i<arr.length; i++) {
+        if(arr[i] > max) {
+            max = arr[i]
+        }
+        if(arr[i] < min) {
+            min = arr[i]
+        }
+    }
+    return [max, min];
+}
+
+const [max, min] = maxMin([5,11,45,4,67,50]);
+console.log(`maximum element ${max}, minimum element ${min}`)

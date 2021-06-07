4076902a2151f726ff6462cff2682281c4fe3708
Abhinav Sancheti <001sancheti@gmail.com>
2021-06-07 18:37:06 +0530

    Kth-smallest-element

--- Array/array-reverse.js
+++ Array/array-reverse.js
@@ -1,10 +1,10 @@
 let reverse = (arr) => {
-    for(let i=0,j=arr.length-1; i<arr.length/2; i++,j--) {
+    for (let i = 0, j = arr.length - 1; i < arr.length / 2; i++, j--) {
         let temp = arr[j];
         arr[j] = arr[i];
         arr[i] = temp;
-        console.log(arr[i])
-    } 
+        console.log(arr[i]);
+    }
     return arr;
-}
-console.log(reverse([1,2,3,4,5]))
+};
+console.log(reverse([1, 2, 3, 4, 5]));
--- Array/maximum-minimum.js
+++ Array/maximum-minimum.js
@@ -1,16 +1,16 @@
 var maxMin = (arr) => {
     let max = arr[0];
     let min = arr[0];
-    for(let i=0; i<arr.length; i++) {
-        if(arr[i] > max) {
-            max = arr[i]
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
         }
-        if(arr[i] < min) {
-            min = arr[i]
+        if (arr[i] < min) {
+            min = arr[i];
         }
     }
     return [max, min];
-}
+};
 
-const [max, min] = maxMin([5,11,45,4,67,50]);
-console.log(`maximum element ${max}, minimum element ${min}`)
+const [max, min] = maxMin([5, 11, 45, 4, 67, 50]);
+console.log(`maximum element ${max}, minimum element ${min}`);
